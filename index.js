c37fc15b22598df4e61961d98a033eaeeaa6aee0
danielaqasha <114327068+danielaqasha@users.noreply.github.com>
2025-04-05 01:48:29 +0900

    Initial Commit

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get('/produk', async (req, res) => {
   try {
